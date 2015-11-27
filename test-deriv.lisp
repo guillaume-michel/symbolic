f41bb0ee464fedaeb6ac0126255b273aef8bb7f0
Guillaume MICHEL <guillaume.michel@orilla.fr>
2015-11-27 23:44:31 +0100

    - Extend derivation to handle sums and products of arbitrary numbers of (two or more) terms

--- test-deriv.lisp
+++ test-deriv.lisp
@@ -7,3 +7,11 @@
 (symbolic:deriv '(* (* x y) (+ x 3)) 'x)
 
 (symbolic:deriv '(+ x x) 'x)
+
+(symbolic:deriv '(* x y (+ x 3)) 'x)
+
+(symbolic:deriv '(* x y z) 'x)
+
+(symbolic:deriv '(+ x y 0) 'x)
+
+(symbolic:deriv '(+ x y 0 z x) 'x)
